bbda71124fac54d8c15aaf866c39e2d3ec0624ba
Shruthum <duttashruthum248@gmail.com>
2025-02-18 14:53:06 +0530

    implementating the service for minio

--- src/main/java/com/ciphershare/v1/configuration/MinIOConfig.java
+++ src/main/java/com/ciphershare/v1/configuration/MinIOConfig.java
@@ -3,7 +3,6 @@ package com.ciphershare.v1.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.minio.MinioClient;
 
 @Configuration
@@ -11,6 +10,7 @@ public class MinIOConfig {
 
     @Autowired
     private MinIOConfigProperties minIOConfigProperties;
+
     @Bean
     public MinioClient minIOClient(){
         return MinioClient.builder().endpoint(minIOConfigProperties.getMiniourl()).credentials(minIOConfigProperties.getAccessKey(),minIOConfigProperties.getSecretKey()).build();
--- src/main/java/com/ciphershare/v1/service/MinioService.java
+++ src/main/java/com/ciphershare/v1/service/MinioService.java
@@ -1,6 +1,64 @@
 package com.ciphershare.v1.service;
 
+import java.io.InputStream;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.ciphershare.v1.configuration.MinIOConfigProperties;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+
+@Service
 public class MinioService {
+
+    @Autowired
+    private MinioClient minioClient;
+    private MinIOConfigProperties minIOConfigProperties;
+    private final String bucketName = minIOConfigProperties.getBucketName();
+
+    public String uploadFile(MultipartFile file){
+        try{
+            String filename = UUID.randomUUID()+"_"+ file.getOriginalFilename();
+            minioClient.putObject(
+                PutObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(filename)
+                    .stream(file.getInputStream(), file.getSize(), -1)
+                    .build()
+            );
+            return filename;
+        }catch(Exception e){
+            throw new RuntimeException("Error Uploading file: "+e.getMessage());
+        }
+    }
+
+    public InputStream downloadFile(String fileName){
+        try {
+            return minioClient.getObject(
+                GetObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(fileName)
+                    .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Error while downloading file "+e.getMessage());
+        }
+    }
+    public void deleteFile(String fileName){
+        try {
+            minioClient.removeObject(
+                RemoveObjectArgs.builder()
+                    .bucket(bucketName)
+                    .object(fileName)
+                    .build()
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Error while deleting file "+e.getMessage());
+        }
+    }
 
 }

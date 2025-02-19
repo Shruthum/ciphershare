5b680c73b2584d454dbd4434879324436e1c73bd
Shruthum <duttashruthum248@gmail.com>
2025-02-19 14:39:43 +0530

    updating the role with enum by replacing the string

--- src/main/java/com/ciphershare/v1/entity/User.java
+++ src/main/java/com/ciphershare/v1/entity/User.java
@@ -1,6 +1,8 @@
 package com.ciphershare.v1.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +26,11 @@ public class User {
     private String username; //username for the backend and for the services
     private String name; //Name of the user
     private String password; //password has to be hashed before being stored in the database
-    private String role; //TEMP,USER,ADMIN
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    public enum Role {
+        ADMIN,USER
+    }
+    //TEMP,USER,ADMIN
     // TEMP will special user who want to share the data with limit of like 50MB
 }

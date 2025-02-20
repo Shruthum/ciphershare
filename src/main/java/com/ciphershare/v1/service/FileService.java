9fcfbb764de4b65725201b4fa68a2bb68879f2b7
Shruthum <duttashruthum248@gmail.com>
2025-02-20 12:50:54 +0530

    added store new version func and rollback to version func

--- src/main/java/com/ciphershare/v1/service/FileService.java
+++ src/main/java/com/ciphershare/v1/service/FileService.java
@@ -6,12 +6,14 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ciphershare.v1.entity.FileMetaData;
+import com.ciphershare.v1.entity.FileVersion;
 import com.ciphershare.v1.repository.FileMetaDataRepository;
 
 import io.minio.MinioClient;
@@ -39,14 +41,7 @@ public class FileService {
 
         String filename = file.getOriginalFilename();
 
-        minioClient.putObject(
-                PutObjectArgs.builder()
-                .bucket(bucketName)
-                .object(filename)
-                .stream(inputStream, file.getSize(), -1)
-                .contentType(file.getContentType())
-                .build()
-                );
+        putInMinIO(bucketName, filename, inputStream, filename, file.getSize());
 
         FileMetaData filemetaData = new FileMetaData();
         filemetaData.setFileName(filename);
@@ -66,5 +61,68 @@ public class FileService {
         );
         fileMetaData.getSharedwithUsers().add(username);
         fileMetaDataRepository.save(fileMetaData);
+    }
+
+    public FileMetaData storeNewVersion(Long fileId,MultipartFile file,String bucketName) throws Exception {
+
+        FileMetaData fileMetaData = fileMetaDataRepository.findById(fileId).orElseThrow(
+            () -> new RuntimeException("File not found!")
+        );
+
+        int newVersion = fileMetaData.getVersions().size() + 1;
+
+        String newFileName = fileMetaData.getFileName()+"__v"+newVersion;
+
+        byte[] encryptedData = encryptionService.encrypt(file.getBytes( ));
+        Path filePath = storageDirectory.resolve(newFileName+".cipher");
+        Files.write(filePath,encryptedData);
+
+        InputStream inputStream = new BufferedInputStream(new ByteArrayInputStream(encryptedData));
+
+        putInMinIO(bucketName,newFileName,inputStream,file.getContentType(),file.getSize());
+
+        FileVersion version = new FileVersion();
+
+        version.setFileMetaData(fileMetaData);
+        version.setVersionNumber(newVersion);
+        version.setStoragePath(filePath.toString());
+        version.setCreatedAt(LocalDateTime.now());
+
+        fileMetaData.addNewVersionOfFile(version);
+        fileMetaDataRepository.save(fileMetaData);
+
+        return fileMetaData;
+    }
+
+    public void rollbackFileVersion(Long fileId,int version){
+
+        FileMetaData fileMetaData = fileMetaDataRepository.findById(fileId).orElseThrow(
+            () -> new RuntimeException("File not found!")
+        );
+
+        FileVersion versionToRestore = fileMetaData.getVersions()
+                                                .stream()
+                                                .filter(fileversion -> fileversion.getVersionNumber() == version)
+                                                .findFirst()
+                                                .orElseThrow(() -> new RuntimeException("Version for the file not found!"));
+
+         fileMetaData.setFileName(versionToRestore.getStoragePath());
+         fileMetaDataRepository.save(fileMetaData);
+    }
+
+    private void putInMinIO(String bucketName,String fileName,InputStream inputStream,String fileType,long fileSize){
+       try{
+           minioClient.putObject(
+                       PutObjectArgs.builder()
+                       .bucket(bucketName)
+                       .object(fileName)
+                       .stream(inputStream,fileSize, -1)
+                       .contentType(fileType)
+                       .build()
+                       );
+        }
+        catch(Exception e){
+            throw new RuntimeException("Cannot store the file");
+        }
     }
 }

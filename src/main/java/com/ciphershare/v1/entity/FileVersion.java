22b6189c36bdea32899bb017d08e2257a6b8ee1c
Shruthum <duttashruthum248@gmail.com>
2025-02-20 10:43:45 +0530

    added filversion

---
+++ src/main/java/com/ciphershare/v1/entity/FileVersion.java
@@ -0,0 +1,32 @@
+package com.ciphershare.v1.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "file_version")
+@AllArgsConstructor
+public class FileVersion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fileVersionId;
+
+    @ManyToOne
+    private FileMetaData fileMetaData;
+
+    private int versionNumber;
+    private String storagePath;
+    private LocalDateTime createdAt;
+}

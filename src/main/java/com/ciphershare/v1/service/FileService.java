19cf26b6c890b00bae1489357a0465810aef79d1
Shruthum <duttashruthum248@gmail.com>
2025-02-20 07:50:44 +0530

    minor changes minioputobject for storing encrypted data and updating the minioservice with less code on upload function

--- src/main/java/com/ciphershare/v1/service/FileService.java
+++ src/main/java/com/ciphershare/v1/service/FileService.java
@@ -1,5 +1,8 @@
 package com.ciphershare.v1.service;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +14,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.ciphershare.v1.entity.FileMetaData;
 import com.ciphershare.v1.repository.FileMetaDataRepository;
 
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+
 @Service
 public class FileService {
 
@@ -18,15 +24,39 @@ public class FileService {
     private EncryptionService encryptionService;
     @Autowired
     private FileMetaDataRepository fileMetaDataRepository;
+    @Autowired
+    private MinioClient minioClient;
 
     private final Path storageDirectory = Paths.get("storage/");
 
-    public String storeFile(MultipartFile file) throws Exception {
+    public FileMetaData storeFile(MultipartFile file,String username,String bucketName) throws Exception {
 
         byte[] encryptedData = encryptionService.encrypt(file.getBytes());
         Path filePath = storageDirectory.resolve(file.getOriginalFilename()+".cipher");
         Files.write(filePath,encryptedData);
-        return filePath.toString();
+
+        InputStream inputStream = new BufferedInputStream(new ByteArrayInputStream(encryptedData));
+
+        String filename = file.getOriginalFilename();
+
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                .bucket(bucketName)
+                .object(filename)
+                .stream(inputStream, file.getSize(), -1)
+                .contentType(file.getContentType())
+                .build()
+                );
+
+        FileMetaData filemetaData = new FileMetaData();
+        filemetaData.setFileName(filename);
+        filemetaData.setFileSize(file.getSize());
+        filemetaData.setFileType(file.getContentType());
+        filemetaData.setUploadedBy(username);
+        filemetaData.setStoragePath(filePath.toString());
+
+        fileMetaDataRepository.save(filemetaData);
+        return filemetaData;
 
     }
 
--- src/main/java/com/ciphershare/v1/service/MinioService.java
+++ src/main/java/com/ciphershare/v1/service/MinioService.java
@@ -1,18 +1,15 @@
 package com.ciphershare.v1.service;
 
 import java.io.InputStream;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import com.ciphershare.v1.configuration.MinIOConfigProperties;
 import com.ciphershare.v1.entity.FileMetaData;
-import com.ciphershare.v1.repository.FileMetaDataRepository;
 
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
 import io.minio.RemoveObjectArgs;
 
 @Service
@@ -21,36 +18,19 @@ public class MinioService {
     @Autowired
     private MinioClient minioClient;
     @Autowired
-    private FileMetaDataRepository fileMetaDataRepository;
-    @Autowired
     private FileSearchService fileSearchService;
+    @Autowired
+    private FileService filePathStorageService;
+
 
     private MinIOConfigProperties minIOConfigProperties;
     private final String bucketName = minIOConfigProperties.getBucketName();
 
-    public String uploadFile(MultipartFile file,String username){
+    public void uploadFile(MultipartFile file,String username){
         try{
-            String filename = UUID.randomUUID()+"_"+ file.getOriginalFilename();
-            minioClient.putObject(
-                PutObjectArgs.builder()
-                    .bucket(bucketName)
-                    .object(filename)
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .contentType(file.getContentType())
-                    .build()
-            );
 
-            FileMetaData filemetaData = new FileMetaData();
-            filemetaData.setFileName(filename);
-            filemetaData.setFileSize(file.getSize());
-            filemetaData.setFileType(file.getContentType());
-            filemetaData.setUploadedBy(username);
-            filemetaData.setStoragePath(filename);
-
-            fileMetaDataRepository.save(filemetaData);
+            FileMetaData filemetaData = filePathStorageService.storeFile(file,username,bucketName);
             fileSearchService.indexFileMetaData(filemetaData);
-
-            return filename;
 
         }catch(Exception e){
             throw new RuntimeException("Error Uploading file: "+e.getMessage());
@@ -58,6 +38,7 @@ public class MinioService {
     }
 
     public InputStream downloadFile(String fileName){
+        // This function will return encrypt data
         try {
             return minioClient.getObject(
                 GetObjectArgs.builder()

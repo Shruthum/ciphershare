2ce2f83e2c5a625cc752bc5c534bd97122ce096d
Shruthum <duttashruthum248@gmail.com>
2025-02-21 23:39:16 +0530

    add redis reactive and integrating websocket broker for real time notifications

--- src/main/java/com/ciphershare/v1/configuration/WebSocketConfig.java
+++ src/main/java/com/ciphershare/v1/configuration/WebSocketConfig.java
@@ -1,15 +1,20 @@
 package com.ciphershare.v1.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import com.ciphershare.v1.service.NotificationHandler;
 
 
 @EnableWebSocket
+@EnableWebSocketMessageBroker
 @Configuration
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketConfigurer,WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -17,4 +22,16 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(new NotificationHandler(),"/ws/notifications").setAllowedOrigins("*");
     }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/live");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
 }

cc04c6b26ad57a3775c40ef148125b3ae10633e8
Shruthum <duttashruthum248@gmail.com>
2025-02-22 08:27:53 +0530

    creating for real time notifications of collaboration

--- src/main/java/com/ciphershare/v1/configuration/WebSocketConfig.java
+++ src/main/java/com/ciphershare/v1/configuration/WebSocketConfig.java
@@ -2,25 +2,13 @@ package com.ciphershare.v1.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import com.ciphershare.v1.service.NotificationHandler;
 
-
-@EnableWebSocket
 @EnableWebSocketMessageBroker
 @Configuration
-public class WebSocketConfig implements WebSocketConfigurer,WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
-        registry.addHandler(new NotificationHandler(),"/ws/notifications").setAllowedOrigins("*");
-    }
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
